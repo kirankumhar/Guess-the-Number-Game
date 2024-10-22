44f701b9d1ca728ad73c7e656a269386e5d99b1c
kiran kumhar <kiran.kumar91@live.com>
2024-10-22 18:27:22 +0530

    guess a number

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-alert("Welcome to my js 1st project");
+let userConsent = confirm("Welcome to 'Guess A Number game'");
+
+if(userConsent){
+
+    let computerNumber = Math.ceil(Math.random()*10);
+    console.log(computerNumber);
+    let counter = 0
+    while(counter<3){
+        let userGuessed = prompt("Please enter a Number");
+        if(userGuessed == computerNumber){
+            alert("Awesome you guessed right number");
+            break
+        }else{
+            alert("Sorry Wrong number")
+        }
+        counter+= 1
+    }
+    if(counter == 3){
+        alert("Sorry only 3 guesses allowed");
+    }
+
+}else{
+    alert("Ok! May be next time");
+}
+
+
+// console.log(ranNumber);
+
